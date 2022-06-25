cd0849a0c3ee9a63b496adadd8f1507621476b4f
Hiteshu Jani <hiteshujani2002@gmail.com>
2022-06-25 17:36:40 +0530

    further changes

---
+++ pages/question/[id].js
@@ -0,0 +1,134 @@
+import React from 'react'
+import { useRouter , useState } from 'next/router'
+
+const getQuestion = (questions, index) => {
+  return questions,index;
+};
+export async function getStaticProps({params}) {
+  const quiz = await getById(params.id);
+  return {
+      props: {
+          quiz,
+      }
+  }
+}
+
+export async function getStaticPaths() {
+  const quizzes = await getAll();
+  const paths = quizzes.data.map(quiz => ({params: {id: `${quiz.id}`}}));
+  return {
+      paths,
+      fallback: false
+  }
+}
+
+
+export const Id = ({quiz}) => {
+    const router = useRouter()
+    const {id} = router.query
+    const [index, setIndex] = React.useState(0);
+    const question = getQuestion(quiz.questions, index);
+    const hasNext = () => {
+      return index < quiz.questions.length - 1;
+    };
+    const nextQuestion = () => {
+      if (!hasNext()) {
+          finishQuiz();
+      } else {
+          setIndex(index + 1);
+      }
+    };
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="text-center mb-20">
+      <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">{}</h1>
+      <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
+    </div>
+    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
+          <span className="title-font font-medium">Authentic Cliche Forage</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
+          <span className="title-font font-medium">Kinfolk Chips Snackwave</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
+          <span className="title-font font-medium">Coloring Book Ethical</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
+          <span className="title-font font-medium">Typewriter Polaroid Cray</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
+          <span className="title-font font-medium">Pack Truffaut Blue</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-100 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="3" className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
+          <span className="title-font font-medium">The Catcher In The Rye</span>
+        </div>
+      </div>
+    </div>
+    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+  </div>
+</section>
+    </div>
+  )
+}
+
+
+// export async function getServerSideProps(context) {
+//   console.log(context.query)
+//   let a = await fetch("http://localhost:1337/api/questions?filters[slug]="+ context.query.slug)
+//   let question = await a.json()
+//   return {
+//     props: {question:question.data[0]}, // will be passed to the page component as props
+//   }
+// }
+const QUIZ_URLS = {
+  get: 'http://localhost:1337/api/questions?populate=*',
+};
+export const getAll = async () => {
+  const res = await fetch(QUIZ_URLS.get);
+  return await res.json();
+};
+
+export const getById = async (id) => {
+  const res = await fetch(`${QUIZ_URLS.get}/${id}`);
+  return await res.json();
+};
+
+export default Id
