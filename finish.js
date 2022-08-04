1125fd51f2c796fa1c68e33027a8f55219abc5f0
Hiteshu Jani <84509735+hiteshujani2002@users.noreply.github.com>
2022-08-04 18:25:03 +0530

    We Work further changes

---
+++ finish.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export const finish = () => {
+  return (
+    
+  )
+}
+export default finish;
