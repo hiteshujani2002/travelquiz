d67f66739d2829b3a930ba338d53bf4a9d490c7d
Hiteshu Jani <84509735+hiteshujani2002@users.noreply.github.com>
2022-10-05 15:34:24 +0530

    Add files via upload

---
+++ finish.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export const finish = () => {
+  return (
+    <div>
+      <h2>Hi There</h2>
+    </div>
+  )
+}
+export default finish;
