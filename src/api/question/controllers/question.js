ad5feaf1b1c99bfd0ef94daf871c2628e28f8d25
Hiteshu Jani <84509735+hiteshujani2002@users.noreply.github.com>
2022-10-27 11:25:26 +0530

    Update question.js

--- src/api/question/controllers/question.js
+++ src/api/question/controllers/question.js
@@ -6,4 +6,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::question.question');
+
+
+module.exports = createCoreController("api::question.question", ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
+  
+      const query = {
+        filters: { slug },
+        ...ctx.query,
+      };
+  
+      const post = await strapi.entityService.findMany("api::question.question",{
+        populate: '*',
+      } ,query);
+
+  
+      return this.transformResponse(post[0]);
+    },
+  }));
