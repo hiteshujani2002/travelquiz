317c2323c5d40016969b0dad490b4b68a089cd59
Hiteshu Jani <84509735+hiteshujani2002@users.noreply.github.com>
2022-10-28 13:54:15 +0530

    Update question.js

--- src/api/question/routes/question.js
+++ src/api/question/routes/question.js
@@ -4,6 +4,50 @@
  * question router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::question.question');
+const { createCoreRouter } = require("@strapi/strapi").factories;
+
+const defaultRouter = createCoreRouter("api::question.question");
+
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+
+      });
+      
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+const myExtraRoutes = [];
+
+const myOverideRoute = [
+  {
+    method: "GET",
+    path: "/questions/:slug",
+    handler: "api::question.question.find",
+  },
+];
+
+module.exports = customRouter(defaultRouter, myOverideRoute, myExtraRoutes);
